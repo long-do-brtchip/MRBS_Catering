86f5c6f06f448f6b6f50cff05192f328cd8d08f9
Do Thanh Long (BRT-VN) <long.do@brtchip.com>
2017-11-28 16:02:14 +0700

    resolve conflict

--- src/entity/auth/employee.ts
+++ src/entity/auth/employee.ts
@@ -1,23 +1,15 @@
 import {IsEmail} from "class-validator";
-<<<<<<< HEAD
-import {BaseEntity, Column, Entity,
-        Index, PrimaryColumn} from "typeorm";
-=======
 import {BaseEntity, Column, Entity, OneToMany,
         PrimaryGeneratedColumn} from "typeorm";
 import {Rfid} from "./rfid";
->>>>>>> - change name file rfidapi.ts to authapi.ts
 
 @Entity()
 export class Employee extends BaseEntity {
-  @PrimaryColumn()
-  @IsEmail()
-  public email: string;
+  @PrimaryGeneratedColumn()
+  public id: number;
 
   @Column()
   public name: string;
-<<<<<<< HEAD
-=======
 
   @Column()
   @IsEmail()
@@ -25,5 +17,4 @@ export class Employee extends BaseEntity {
 
   @OneToMany((type) => Rfid, (rfid) => rfid.employee)
   public rfid: Rfid[];
->>>>>>> - change name file rfidapi.ts to authapi.ts
 }
